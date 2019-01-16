7fd7fcd27b9255a411c9b85cc086446237f9d5d5
wgeorge1990 <williameverettgeorge@icloud.com>
2019-01-16 11:00:45 -0500

    that didnt go well at all

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,19 +2,29 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
-
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  constructor(props) {
+  super(props)
   }
 
-};
+
+renderBotCard = () => {console.log(this.props.allBots)
+  const everybot = this.props.allBots
+  console.log(everybot)
+everybot.map(bot => {<BotCard bot={bot}/>})
+//
+}
+
+
+render() {
+  //console.log(this.props)
+      return (
+    	  <div className="ui four column grid">
+      		<div className="row">
+            {this.renderBotCard()}
+      		</div>
+    	  </div>
+    	);
+    }
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,30 @@
 import React from "react";
+import BotCollection from './BotCollection.js'
+const API = ("https://bot-battler-api.herokuapp.com/api/v1/bots")
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  //start here with your code for step on
+  state = {
+  allBots: this.state
+  
+    }
+
+
+  fetchBots = () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(json => this.setState({allBots: json}))
+    }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
+
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection allBots={this.state.allBots} />
       </div>
     );
   }
