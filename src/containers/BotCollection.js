3e9efbec098896a5ca962433812ec80ec118a2cb
github username <github email address>
2019-01-16 09:58:19 -0500

    fetched and indexed bots to page

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,10 +5,11 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.props.bots.map(bot => <BotCard key={bot.id} bot={bot}/>)}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,32 @@
 import React from "react";
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
+
+
+const API = `https://bot-battler-api.herokuapp.com/api/v1/bots`
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: []
+  }
+
+  fetchBots = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(json => this.setState({bots: json}))
+  }
+
+  componentDidMount(){
+    this.fetchBots()
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+      <BotCollection bots={this.state.bots} />
+      <YourBotArmy />
       </div>
     );
   }
